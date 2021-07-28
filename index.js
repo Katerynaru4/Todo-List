ccc13848a857b023e2ef3f56ff9f511a505a86a4
Katerynaru4 <70796523+Katerynaru4@users.noreply.github.com>
2021-07-28 18:47:12 +0300

    sorting tasks in list

--- index.js
+++ index.js
@@ -12,35 +12,36 @@ const tasks = [
 
 const changeTaskStatus = (id) => {
   tasks[id].done = !tasks[id].done;
-  tasks.sort((a, b) => a.done - b.done);
   listElem.innerHTML = '';
   renderTasks(tasks);
 };
 
 const renderTasks = (tasksList) => {
-  const tasksElems = tasksList.map(({ text, done }, index) => {
-    const listItemElem = document.createElement('li');
-    listItemElem.classList.add('list__item');
+  const tasksElems = tasksList
+    .sort((a, b) => a.done - b.done)
+    .map(({ text, done }, index) => {
+      const listItemElem = document.createElement('li');
+      listItemElem.classList.add('list__item');
 
-    listItemElem.dataset.taskId = index;
-    tasks[index].id = index;
+      listItemElem.dataset.taskId = index;
+      tasks[index].id = index;
 
-    const checkbox = document.createElement('input');
-    checkbox.setAttribute('type', 'checkbox');
-    checkbox.checked = done;
-    checkbox.classList.add('list__item-checkbox');
+      const checkbox = document.createElement('input');
+      checkbox.setAttribute('type', 'checkbox');
+      checkbox.checked = done;
+      checkbox.classList.add('list__item-checkbox');
 
-    checkbox.addEventListener('click', () =>
-      changeTaskStatus(listItemElem.dataset.taskId)
-    );
+      checkbox.addEventListener('click', () =>
+        changeTaskStatus(listItemElem.dataset.taskId)
+      );
 
-    if (done) {
-      listItemElem.classList.add('list__item_done');
-    }
-    listItemElem.append(checkbox, text);
+      if (done) {
+        listItemElem.classList.add('list__item_done');
+      }
+      listItemElem.append(checkbox, text);
 
-    return listItemElem;
-  });
+      return listItemElem;
+    });
 
   listElem.append(...tasksElems);
 };
