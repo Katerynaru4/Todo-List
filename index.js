02417333134b6ca4057df37be49f03a987ea328e
Katerynaru4 <70796523+Katerynaru4@users.noreply.github.com>
2021-08-06 15:26:53 +0300

    sort

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// 'use strict';
+
 const listElem = document.querySelector('.list');
 const taskInput = document.querySelector('.task-input');
 const createTaskBtn = document.querySelector('.create-task-btn');
@@ -5,40 +7,45 @@ const createTaskBtn = document.querySelector('.create-task-btn');
 const tasks = [
   {
     text: '1 Buy milk',
+    id: 0.31709864797951631,
     done: false,
     creationDate: new Date(2021, 08, 06, 9, 45),
   },
   {
     text: '2 Pick up Tom from airport',
+    id: 0.31709840797251632,
     done: false,
     creationDate: new Date(2021, 08, 06, 10, 45),
   },
   {
     text: '3 Visit party',
+    id: 0.31709864797954533,
     done: false,
     creationDate: new Date(2021, 08, 06, 11, 45),
   },
   {
     text: '4 Visit doctor',
+    id: 0.31709864738481634,
     done: false,
     creationDate: new Date(2021, 08, 06, 12, 45),
   },
   {
     text: '5 Buy meat',
+    id: 0.31709454797951635,
     done: false,
     creationDate: new Date(2021, 08, 06, 13, 45),
   },
 ];
 
 const changeTaskStatus = (id) => {
-  tasks[id].done = !tasks[id].done;
-  if (tasks[id].done) {
-    tasks[id].doneDate = new Date();
+  const checkedtask = tasks.find((task) => task.id === Number(id));
+  checkedtask.done = !checkedtask.done;
+  if (checkedtask.done) {
+    checkedtask.doneDate = new Date();
   }
-  if (!tasks[id].done) {
-    delete tasks[id].doneDate;
+  if (!checkedtask.done) {
+    delete checkedtask.doneDate;
   }
-
   listElem.innerHTML = '';
   renderTasks(tasks);
 };
@@ -53,22 +60,15 @@ const renderTasks = (tasksList) => {
       if (a.creationDate - b.creationDate)
         return a.creationDate - b.creationDate;
     })
-    .map(({ text, done }, index) => {
+    .map(({ text, id, done }, index) => {
       const listItemElem = document.createElement('li');
       listItemElem.classList.add('list__item');
 
-      listItemElem.dataset.taskId = index;
-      tasks[index].id = index;
-
       const checkbox = document.createElement('input');
       checkbox.setAttribute('type', 'checkbox');
+      checkbox.dataset.taskId = id;
       checkbox.checked = done;
       checkbox.classList.add('list__item-checkbox');
-
-      checkbox.addEventListener('click', () =>
-        changeTaskStatus(listItemElem.dataset.taskId)
-      );
-
       if (done) {
         listItemElem.classList.add('list__item_done');
       }
@@ -80,10 +80,16 @@ const renderTasks = (tasksList) => {
   listElem.append(...tasksElems);
 };
 
+listElem.addEventListener('click', (event) => {
+  event.stopPropagation();
+  changeTaskStatus(event.target.dataset.taskId);
+});
+
 const addTask = () => {
   if (taskInput.value === '') return;
   tasks.push({
     text: taskInput.value,
+    id: Math.random(),
     done: false,
     date: new Date(),
   });
